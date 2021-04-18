ac20f7144f60c0d88b261ff80a140a297f6db244
Danny Escalante <danny.escalante@multicaja.cl>
2021-04-18 01:22:40 -0400

    update cross Origin para las peticiones

--- src/main/java/cl/descalante/app/clientes/controller/ClienteController.java
+++ src/main/java/cl/descalante/app/clientes/controller/ClienteController.java
@@ -3,17 +3,22 @@ package cl.descalante.app.clientes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.descalante.app.clientes.Entity.Cliente;
 import cl.descalante.app.clientes.Service.IClienteService;
 import cl.descalante.app.clientes.responses.ResponseClienteReceta;
 
+@RequestMapping(value = "/api/v1")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT })
 @RestController
 public class ClienteController {
 	
